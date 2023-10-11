c4de2d75d773a4d2e6ae34bacbc3b8e676952131
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-10-12 00:56:47 +0200

    basic tests done

--- 02-setup/code/basic-testing-frontend/src/util/validation.test.js
+++ 02-setup/code/basic-testing-frontend/src/util/validation.test.js
@@ -1,57 +1,74 @@
-import { it, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 import { validateNumber, validateStringNotEmpty } from './validation.js';
 
-it('Should throw Error when an empty string is is provided', () => {
-	const input = '';
+describe('validateStringNotEmpty()', () => {
+	it('Should throw Error when an empty string is is provided', () => {
+		const input = '';
 
-	const resultFr = () => {
-		validateStringNotEmpty(input);
-	};
+		const resultFr = () => {
+			validateStringNotEmpty(input);
+		};
 
-	expect(resultFr).toThrow();
-});
-it('Should throw Error when string of blonks is provided', () => {
-	const input = '            ';
+		expect(resultFr).toThrow();
+	});
+	it('Should throw Error when string of blonks is provided', () => {
+		const input = '            ';
 
-	const resultFr = () => {
-		validateStringNotEmpty(input);
-	};
+		const resultFr = () => {
+			validateStringNotEmpty(input);
+		};
 
-	expect(resultFr).toThrow();
-});
-it('Should throw Error with a message that contains (must not be empty)', () => {
-	const input = '';
+		expect(resultFr).toThrow();
+	});
+	it('Should throw Error if any other value than string is provided', () => {
+		const inputOne = 1;
+		const inputTwo = false;
+		const inputThree = {};
 
-	const resultFr = () => {
-		validateStringNotEmpty(input);
-	};
+		const validateInputOne = () => validateStringNotEmpty(inputOne);
+		const validateinputTwo = () => validateStringNotEmpty(inputTwo);
+		const validateinputThree = () => validateStringNotEmpty(inputThree);
 
-	expect(resultFr).toThrow(/must not be empty/);
-});
+		expect(validateInputOne).toThrow();
+		expect(validateinputTwo).toThrow();
+		expect(validateinputThree).toThrow();
+	});
+	it('Should throw Error with a message that contains (must not be empty)', () => {
+		const input = '';
 
-it('Should throw Error when an NaN is provided', () => {
-	const input = NaN;
+		const resultFr = () => {
+			validateStringNotEmpty(input);
+		};
 
-	const resultFr = () => {
-		validateNumber(input);
-	};
-
-	expect(resultFr).toThrow();
-});
-it('Should throw Error with a message that contains (Invalid number input)', () => {
-	const input = NaN;
-
-	const resultFr = () => {
-		validateNumber(input);
-	};
-
-	expect(resultFr).toThrow(/Invalid number input./);
+		expect(resultFr).toThrow(/must not be empty/);
+	});
 });
 
-it('Should throw Error when a string number is provided', () => {
-	const input = '1';
+describe('validateNumber()', () => {
+	it('Should throw Error when an NaN is provided', () => {
+		const input = NaN;
 
-	const resultFr = () => validateNumber(input);
+		const resultFr = () => {
+			validateNumber(input);
+		};
 
-	expect(resultFr).toThrow();
+		expect(resultFr).toThrow();
+	});
+	it('Should throw Error with a message that contains (Invalid number input)', () => {
+		const input = NaN;
+
+		const resultFr = () => {
+			validateNumber(input);
+		};
+
+		expect(resultFr).toThrow(/Invalid number input./);
+	});
+
+	it('Should throw Error when a string number is provided', () => {
+		const input = '1';
+
+		const resultFr = () => validateNumber(input);
+
+		expect(resultFr).toThrow();
+	});
 });
