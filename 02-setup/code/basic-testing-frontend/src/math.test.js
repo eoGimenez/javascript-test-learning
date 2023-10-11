c5b5c61e1d98ac83ba8cb7565585b923868518d0
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-10-11 14:05:43 +0200

    unit test

--- 02-setup/code/basic-testing-frontend/src/math.test.js
+++ 02-setup/code/basic-testing-frontend/src/math.test.js
@@ -1,12 +1,20 @@
 import { expect, it } from 'vitest';
-import { add } from './math.js'
+import { add } from './math.js';
 
 it('should summarize all number values in an array', () => {
-    const result = add([1,4])
-    expect(result).toBe(5)
+	const numbers = [1, 4];
+    
+	const result = add(numbers);
+
+	const expectedResult = numbers.reduce((a, b) => a + b, 0);
+	expect(result).toBe(expectedResult);
 });
 
 it('result should be: -2', () => {
-    const result = add([1,-3])
-    expect(result).toBe(-2)
-})
+	const numbers = [1, -3];
+
+	const result = add(numbers);
+
+	const expectedResult = numbers.reduce((a, b) => a + b, 0);
+	expect(result).toBe(expectedResult);
+});
