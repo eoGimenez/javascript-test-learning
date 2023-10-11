6879fd5de5ca3f37c4e45299e49c72639ec6d9b9
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-10-11 14:17:52 +0200

    arreglando NaN

--- 02-setup/code/basic-testing-frontend/src/math.test.js
+++ 02-setup/code/basic-testing-frontend/src/math.test.js
@@ -3,18 +3,35 @@ import { add } from './math.js';
 
 it('should summarize all number values in an array', () => {
 	const numbers = [1, 4];
-    
+
 	const result = add(numbers);
 
 	const expectedResult = numbers.reduce((a, b) => a + b, 0);
 	expect(result).toBe(expectedResult);
 });
 
-it('result should be: -2', () => {
+it('result should be negative', () => {
 	const numbers = [1, -3];
 
 	const result = add(numbers);
 
 	const expectedResult = numbers.reduce((a, b) => a + b, 0);
+	expect(result).toBe(expectedResult);
+});
+
+it('should yield NaN if at least one invalid number is provided', () => {
+	const inputs = [1, 'invalid number', 5];
+
+	const result = add(inputs);
+
+	expect(result).toBeNaN();
+});
+
+it('should yield a number', () => {
+	const inputs = ['1', '3'];
+
+	const result = add(inputs);
+
+	const expectedResult = inputs.reduce((a, b) => +a + +b, 0);
 	expect(result).toBe(expectedResult);
 });
