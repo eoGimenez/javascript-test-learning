c1c92f9bcc8807efa115a45a8f8eab55a25e0407
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-10-13 12:51:33 +0200

    esctructura de testeo para asyn/await y promesas

---
+++ 03-Advanced/code/async/async-example.test.js
@@ -0,0 +1,41 @@
+import { describe, it, expect } from 'vitest';
+import { generateToken, generateTokenPromise } from './async-example';
+
+describe('generateToken()', () => {
+	it('Should generate a token value', (done) => {
+		const userEmail = 'a@a.com';
+
+		generateToken(userEmail, (err, token) => {
+			// usando try/catch, podemos ver que error recivimos si falla
+			try {
+				expect(token).toBeDefined();
+				done(); // de esta forma el test espera la función asíncrona.
+			} catch (err) {
+				done(err);
+			}
+
+			// si queremos comparar el valor que devuelve hay que usar try/catch:
+			// try {
+			// 	expect(token).toBe(2);
+			// 	done();
+			// } catch (err) {
+			// 	done(err);
+			// }
+			// así comparamos que valor recivimos de la función asíncrona
+		});
+	});
+});
+
+describe('generateTokenPromise()', () => {
+	it('Should generate a token value', () => {
+		const userEmail = 'a@a.com';
+
+		expect(generateTokenPromise(userEmail)).resolves.toBeDefined();
+	});
+	it('Should generate a token value, usando Async/await', async () => {
+		const userEmail = 'a@a.com';
+
+		const token = await generateTokenPromise(userEmail);
+		expect(token).toBeDefined();
+	});
+});
