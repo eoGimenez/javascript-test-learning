72a7139fff39759f22aa94feb3eb7cb09d2b4efa
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-10-15 10:40:13 +0200

    Refactorizando hook.test, aunque hay quienes no dan validas las variables globales en los test, usando hooks es mucho mas clean

--- 03-Advanced/code/hooks/hooks.test.js
+++ 03-Advanced/code/hooks/hooks.test.js
@@ -1,54 +1,57 @@
-import { it, expect } from 'vitest';
-
+import { it, expect, beforeAll, beforeEach, afterAll, afterEach } from 'vitest';
 import { User } from './hooks';
 
+let user;
+const testEmail = 'test@test.com';
+// let user = new User(testEmail) <<< su eligieramos el afterEach()
+
+// los hooks de inicialización:
+beforeAll(() => {
+	console.log('BeforeAll()');
+});
+
+beforeEach(() => {
+	user = new User(testEmail);
+});
+
+afterAll(() => {
+	console.log('BeforeAll()');
+});
+
+afterEach(() => {
+	// user = new User(testEmail); <<< Tambien funciona, aunque asi,
+	// tendriamos que inicializar "user" en global o en beforeAll() para el primer test
+	console.log('BeforeEach()');
+});
+
 it('Should update the email', () => {
-	const testEmail = 'test@test.com';
 	const newTestEmail = 'test2@test.com';
 
-	const user = new User(testEmail);
 	user.updateEmail(newTestEmail);
 
 	expect(user.email).toBe(newTestEmail);
 });
 
 it('Should have an email property', () => {
-	const testEmail = 'test@test.com';
-
-	const user = new User(testEmail);
-
 	expect(user).toHaveProperty('email');
 });
 
 it('Should store the provided email value', () => {
-	const testEmail = 'test@test.com';
-
-	const user = new User(testEmail);
-
 	expect(user.email).toBe(testEmail);
 });
 
 it('Should clear the email', () => {
-	const testEmail = 'test@test.com';
-
-	const user = new User(testEmail);
 	user.clearEmail();
 
 	expect(user.email).toBe('');
 });
 
 it('Should still have an email property after clearing the email', () => {
-	const testEmail = 'test@test.com';
-
-	const user = new User(testEmail);
 	user.clearEmail();
 
 	expect(user).toHaveProperty('email');
 });
 it('Should have an empty email property after clearing the email', () => {
-	const testEmail = 'test@test.com';
-
-	const user = new User(testEmail);
 	user.clearEmail();
 
 	expect(user.email).toBe('');
