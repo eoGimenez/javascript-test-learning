25b75b68c18de3732710a8afd1479852c84e6be3
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-10-17 20:52:12 +0200

    correcciones

--- 04-Mocks-Spies/src/data.test.js
+++ 04-Mocks-Spies/src/data.test.js
@@ -2,18 +2,16 @@ import { describe, expect, it, vi } from 'vitest';
 import { generateReportData, storeData } from './data';
 
 describe.concurrent('generateReportData()', () => {
-
-    // Ejemplo de Spies:
+	// Ejemplo de Spies:
 	it('Should execute logFn() if its provided', () => {
-        // con el objeto vi, y su metodo fn() creamos una funcion vacia que almacena si es usada,
-        // es el espia que pasaremos como "spy" y luego miramos si en la funcion principal fue llamada
+		// con el objeto vi, y su metodo fn() creamos una funcion vacia que almacena si es usada,
+		// es el espia que pasaremos como "spy" y luego miramos si en la funcion principal fue llamada
 		const logger = vi.fn();
 
 		generateReportData(logger);
 
 		expect(logger).toBeCalled();
 	});
-
 });
 
 // describe.concurrent('storeData()', () => {});
