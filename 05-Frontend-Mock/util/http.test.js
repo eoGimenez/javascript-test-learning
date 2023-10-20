0ea406917ad743a9faa8742f808a825c362460d1
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-10-20 13:29:13 +0200

    cambiando funcionalidades del mock para algunos tests en particular

--- 05-Frontend-Mock/util/http.test.js
+++ 05-Frontend-Mock/util/http.test.js
@@ -1,10 +1,15 @@
 import { expect, it, vi } from 'vitest';
 import { sendDataRequest } from './http';
+import { HttpError } from './errors';
 
+const testData = { key: 'test' };
 const testResponseData = { keyResponse: 'response test fetch' };
 
 const testFetch = vi.fn((url, options) => {
 	return new Promise((resolve, reject) => {
+		if (typeof options.body !== 'string') {
+			return reject('Not a string.');
+		}
 		const testResponse = {
 			ok: true,
 			json() {
@@ -20,7 +25,38 @@ const testFetch = vi.fn((url, options) => {
 vi.stubGlobal('fetch', testFetch);
 
 it('Should return any available response data', () => {
-	const testData = { key: 'test' };
+	return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+});
 
-	return expect(sendDataRequest(testData)).resolves.toBe(testResponseData);
+it('Should convert the provided data to JSON before sending the request', async () => {
+	let errorMessage;
+
+	try {
+		await sendDataRequest(testData);
+	} catch (err) {
+		errorMessage = err;
+	}
+
+	expect(errorMessage).not.toBe('Not a string.');
+});
+
+it('Should throw a HttpError in case of non-ok responses', () => {
+	testFetch.mockImplementationOnce((url, options) => {
+		return new Promise((resolve, reject) => {
+			if (typeof options.body !== 'string') {
+				return reject('Not a string.');
+			}
+			const testResponse = {
+				ok: false,
+				json() {
+					return new Promise((resolve, reject) => {
+						resolve(testResponseData);
+					});
+				},
+			};
+			resolve(testResponse);
+		});
+	});
+
+	return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError);
 });
